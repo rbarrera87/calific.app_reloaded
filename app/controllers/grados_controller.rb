44a581ea18702b5c2f19ba8a1313f2e1df4b6050
Miguel Marroquin <marroquin.em93@gmail.com>
2014-02-26 14:41:52 -0600

    Updated-Views-Of-Grade-Shell

--- app/controllers/grados_controller.rb
+++ app/controllers/grados_controller.rb
@@ -1,6 +1,8 @@
 class GradosController < ApplicationController
- before_action :set_grado, only: [:show, :edit, :update, :destroy]
+  before_action :set_grado, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!
+  layout 'application'
+
   def index
     @grados = Grado.all
   end
@@ -13,12 +15,12 @@ class GradosController < ApplicationController
   end
 
   def edit
-
   end
 
   def create
-     @grado = Grado.new(grado_params)
-     respond_to do |format|
+    @grado = Grado.new(grado_params)
+
+    respond_to do |format|
       if @grado.save
         format.html { redirect_to @grado, notice: 'El grado fue agregado.' }
         format.json { render action: 'show', status: :created, location: @grado }
@@ -28,8 +30,9 @@ class GradosController < ApplicationController
       end
     end
   end
-def update
-     respond_to do |format|
+
+  def update
+    respond_to do |format|
       if @grado.update(grado_params)
         format.html { redirect_to @grado, notice: 'El grado se ha actualizado.' }
         format.json { head :no_content }
@@ -39,20 +42,22 @@ def update
       end
     end
   end
+
   def destroy
     @grado.destroy
     respond_to do |format|
       format.html {redirect_to grados_url}
       format.json {head :no_content}
+    end
   end
-end
 
-private
+  private
     # Use callbacks to share common setup or constraints between actions.
     def set_grado
       @grado = Grado.find(params[:id])
     end
-# Never trust parameters from the scary internet, only allow the white list through.
+    
+    # Never trust parameters from the scary internet, only allow the white list through.
     def grado_params
       params.require(:grado).permit(:nombre, :descripcion)
     end
