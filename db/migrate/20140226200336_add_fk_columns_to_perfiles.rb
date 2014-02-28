1e1261dfc734688a55f6918bfcc35ac3fdf2b352
tony15501 <chekoferreiro@gmail.com>
2014-02-28 16:22:08 -0600

    fixed migration-add_fk_columns_to_perfiles Delete add_change_to_perfiles.rb

--- db/migrate/20140226200336_add_fk_columns_to_perfiles.rb
+++ db/migrate/20140226200336_add_fk_columns_to_perfiles.rb
@@ -1,13 +1,9 @@
 class AddFkColumnsToPerfiles < ActiveRecord::Migration
   def change
-    add_column :perfiles, :user_id, :integer
-    add_column :perfiles, :grupo_id, :integer
-    add_column :perfiles, :carrera_id, :integer
-    change_column_null :perfiles, :user_id,  false
-  	change_column_null :perfiles, :grupo_id, false
-  	change_column_null :perfiles, :carrera_id, false
+add_column :perfiles, :user_id, :integer, null: false
+add_column :perfiles, :grupo_id, :integer, null: false
+add_column :perfiles, :carrera_id, :integer, null: false
+add_index  :perfiles, [:user_id, :grupo_id, :carrera_id], unique: true
+  end
+  end
 
-  	add_index  :perfiles, [:user_id, :grupo_id, :carrera_id], unique: true 
-  end
-  end
-   
