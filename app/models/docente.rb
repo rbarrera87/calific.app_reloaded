b75c0f222eb6775e326d6e093c6425c09e1fbbe6
tony15501 <chekoferreiro@gmail.com>
2014-03-07 19:26:47 -0600

    add relationship model docente- carrera-perfil

--- app/models/docente.rb
+++ app/models/docente.rb
@@ -1,2 +1,6 @@
 class Docente < ActiveRecord::Base
+	self.table_name = "docentes"
+	belongs_to :carrera
+  has_one :perfil
+
 end
--- app/models/perfil.rb
+++ app/models/perfil.rb
@@ -3,11 +3,11 @@ class Perfil < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :carrera
 	belongs_to :grupo
+  belongs_to :docente
 
-	 
  validates :fotografia_url, :numero_control, :fecha_nacimiento, :estado_civil, :sexo,
            :rfc, :curp, :calle,:poblacion, :estado, :situacion, :turno, presence: true
  validates :cp, :nss,  numericality: true
  validates :nombre, :apellido_materno, :apellido_materno, length: { maximum: 30 }
 
-end 
+end
