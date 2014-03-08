bcc406cdd887e593480862c4e40c5b2522bffa30
tony15501 <chekoferreiro@gmail.com>
2014-03-07 20:16:51 -0600

    eliminados

--- db/migrate/20140307181747_add_field_docente_id_to_perfil_carrera_grado_grupo.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldDocenteIdToPerfilCarreraGradoGrupo < ActiveRecord::Migration
-  def change
-  	add_column :perfiles, :docente_id, :integer,null: false
-  	add_column :carreras, :docente_id, :integer,null: false
-  	add_column :grados, :docente_id, :integer,null: false
-  	add_column :grupos, :docente_id, :integer,null: false
-  end
-end
--- db/migrate/20140307182807_add_index_fields_docente_id_to_perfiles_carreras_grupos_grados.rb
+++
@@ -1,8 +0,0 @@
-class AddIndexFieldsDocenteIdToPerfilesCarrerasGruposGrados < ActiveRecord::Migration
-  def change
-  	add_index  :perfiles, :docente_id, unique: true
-  	add_index  :carreras, :docente_id, unique: true
-  	add_index  :grupos, :docente_id, unique: true
-  	add_index  :grados, :docente_id, unique: true
-  	  end
-end
