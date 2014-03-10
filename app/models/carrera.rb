374992b11a062d82284a7208d0b56260f0ef8cec
Miguel Marroquin <marroquin.em93@gmail.com>
2014-03-10 15:46:45 -0600

    Solver conflicts

--- app/models/carrera.rb
+++ app/models/carrera.rb
@@ -1,10 +1,10 @@
 class Carrera < ActiveRecord::Base
 	self.table_name = "carreras"
-		
-		has_many :perfiles
-		has_one :consejero	
-		
-		validates_presence_of :nombre
+
+	has_many :perfiles
+	has_one :consejero	
+
+	validates_presence_of :nombre
 
 
 end
