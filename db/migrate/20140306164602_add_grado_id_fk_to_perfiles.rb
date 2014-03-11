f5bd21b5cce36602f2ac32398909a92b7329cfb5
Miguel Marroquin <marroquin.em93@gmail.com>
2014-03-11 16:38:10 -0600

    Fixed errors

--- db/migrate/20140306164602_add_grado_id_fk_to_perfiles.rb
+++ db/migrate/20140306164602_add_grado_id_fk_to_perfiles.rb
@@ -1,6 +1,6 @@
 class AddGradoIdFkToPerfiles < ActiveRecord::Migration
-  def change
-add_column :perfiles, :grado_id, :integer, null: false
-add_index :perfiles, :grado_id, unique: true
-  end
+	def change
+		add_column :perfiles, :grado_id, :integer, null: false
+		add_index :perfiles, :grado_id, unique: true
+	end
 end
