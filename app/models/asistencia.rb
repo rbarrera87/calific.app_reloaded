77020a8d6364105e90fcd88ef8272f2f5209f146
tony15501 <chekoferreiro@gmail.com>
2014-03-12 13:36:08 -0600

    fixed tab 2

--- app/models/asistencia.rb
+++ app/models/asistencia.rb
@@ -1,6 +1,6 @@
 class Asistencia < ActiveRecord::Base
-	self.table_name = "asistencias"
-	belongs_to :perfil
-	belongs_to :grupo
-	belongs_to :grado
+  self.table_name = "asistencias"
+  belongs_to :perfil
+  belongs_to :grupo
+  belongs_to :grado
 end
