c7c306b11484acdb498f548e4e428a6377e529df
Miguel Marroquin <marroquin.em93@gmail.com>
2014-03-14 11:18:50 -0600

    Fixed errors

--- app/controllers/perfiles_controller.rb
+++ app/controllers/perfiles_controller.rb
@@ -1,12 +1,11 @@
 class PerfilesController < ApplicationController
-  before_action :set_perfil, only: [:show, :edit, :update, :destroy]
+   before_action :set_perfil, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!
   def index
     @perfil = current_user.perfil
   end
 
   def show
-    #render 'show'
   end
 
   def new
@@ -14,35 +13,33 @@ class PerfilesController < ApplicationController
   end
 
   def edit
+  end
 
+  def update
+    respond_to do |format|
+      if @perfil.update(perfil_params)
+        format.html { redirect_to @perfil, notice: 'El perfil se ha actualizado' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @perfil.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def create
-    @perfil = Perfil.new(perfil_params)
-    respond_to do |format|
+     @perfil = Perfil.new(perfil_params)
+      respond_to do |format|
       if @perfil.save
         format.html { redirect_to @perfil, notice: 'El perfil fue agregado.' }
-        format.json { render action: 'show', status: :created, location: @perfil }
+         format.json { render action: 'show', status: :created, location: @perfil }
       else
          format.html { render action: 'new' }
          format.json { render json: @perfil.errors, status: :unprocessable_entity }
       end
      end
   end
-
-    def update
-    respond_to do |format|
-       if @perfil.update(perfil_params)
-          format.html { redirect_to @perfil, notice: 'El perfil se ha actualizado' }
-          format.json { head :no_content }
-       else
-         format.html { render action: 'edit' }
-         format.json { render json: @perfil.errors, status: :unprocessable_entity }
-       end
-    end
-  end
-
-  private
+   private
     # Use callbacks to share common setup or constraints between actions.
     def set_perfil
       @perfil = Perfil.find(params[:id])
@@ -51,7 +48,9 @@ class PerfilesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def perfil_params
       params.require(:perfil).permit(
-       :nombre,
+    
+
+      :nombre,
        :apellido_paterno,
        :apellido_materno,
        :numero_control,
