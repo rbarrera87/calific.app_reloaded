c2f3d5f7ab158aebd4bbfbf0a6d5dc255ca60871
Richard <rbarrera87@gmail.com>
2014-03-27 11:51:44 -0700

    latest changes on migration

--- db/migrate/20140327175103_remove_index_grado_id_to_perfiles.rb
+++ db/migrate/20140327175103_remove_index_grado_id_to_perfiles.rb
@@ -1,5 +1,12 @@
 class RemoveIndexGradoIdToPerfiles < ActiveRecord::Migration
-  def change
-  	remove_index :perfiles, :grado_id
+  def up
+    remove_index :perfiles, :grado_id
+    remove_index :perfiles, [:user_id, :grupo_id, :carrera_id]
+    add_index  :perfiles, [:user_id, :grupo_id, :carrera_id, :grado_id], name: 'constraint_in_perfiles', unique: true
+  end
+  def down
+    add_index :perfiles, :grado_id
+    add_index  :perfiles, [:user_id, :grupo_id, :carrera_id], unique: true
+    remove_index :perfiles, name: :constraint_in_perfiles
   end
 end
